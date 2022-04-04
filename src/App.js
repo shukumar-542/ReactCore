b31917b5dd235e91f8e4d6be3180981e23714cf2
unknown <shukumar-542>
2022-04-04 23:19:47 +0600

    react use state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const products =[
+    {name:'samsung', price:'$200'},
+    {name:'Apple', price:'$400'},
+    {name:'onePlus', price:'$600'},
+    {name:'Oppo', price:'$200'},
+  ]
+  const friends = ['jihad','jafor','kabir','nahid']
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <Count></Count>
+       {
+         products.map(product => <Products pd={product}></Products>)
+       }
+        {
+          friends.map(fr => <ul >{fr}</ul>)
+        }
+
+        
       </header>
     </div>
   );
+}
+
+function Friends(props){
+  
+  return(
+    <div style={{border:'1px solid gold', margin:'5px', width:'500px'}}>
+      
+    </div>
+
+  )
+}
+
+function Count(){
+  const [count, setCount] = useState(0)
+ 
+ 
+  return(
+    <div>Count : {count}
+    <button onClick={()=>setCount(count+1)}>Increse</button>
+    <button onClick={()=> setCount(count -1)}>Decrese</button>
+    </div>
+   
+  )
+}
+
+function Products(props){
+  console.log(props);
+  const productStyle ={
+    border :'1px solid gray',
+    backgroundColor : 'lightgray',
+    borderRadius: '5px',
+    float :'left',
+    width:'200px',
+    height:'230px'
+  }
+  return (
+    <div style={productStyle}>
+      <h2>{props.pd.name}</h2>
+      <h1>{props.pd.price}</h1>
+
+    </div>
+  )
 }
 
 export default App;
